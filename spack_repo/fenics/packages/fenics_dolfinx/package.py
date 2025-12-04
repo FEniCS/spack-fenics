9394ef69ad725238b077a4cda9118a9419cdc3ad
Jack S. Hale <mail@jackhale.co.uk>
2025-12-04 13:39:59 +0100

    Remove main version dependencies for fenics packages
    Removed direct dependencies on 'fenics-ufcx', 'fenics-basix', and 'py-fenics-ffcx' for the 'main' version.

--- spack_repo/fenics/packages/fenics_dolfinx/package.py
+++ spack_repo/fenics/packages/fenics_dolfinx/package.py
@@ -81,11 +81,6 @@ class FenicsDolfinx(CMakePackage):
     depends_on("adios2@2.8.1:", when="@0.9: +adios2")
     depends_on("adios2+mpi", when="+adios2")
 
-    # ensure main matches to main (not recommended version)
-    depends_on("fenics-ufcx@main", when="@main")
-    depends_on("fenics-basix@main", when="@main")
-    depends_on("py-fenics-ffcx@main", when="@main", type="test")
-
     for ver in ("main", "0.10", "0.9", "0.8", "0.7", "0.6"):
         depends_on(f"fenics-ufcx@{ver}", when=f"@{ver}")
         depends_on(f"fenics-basix@{ver}", when=f"@{ver}")
