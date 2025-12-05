a39e0a4994246bff452bf9dc8ba7d6f8655ecbb0
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2025-12-05 17:42:54 +0100

    revert

--- spack_repo/fenics/packages/py_fenics_ufl/package.py
+++ spack_repo/fenics/packages/py_fenics_ufl/package.py
@@ -80,7 +80,6 @@ class PyFenicsUfl(PythonPackage):
 
     @run_after("install")
     @on_package_attributes(run_tests=True)
-    def install_test(self):
-        with working_dir("test"):
-            pytest = which("pytest")
-            pytest()
+    def check_build(self):
+        with working_dir(self.stage.source_path):
+            python("-m", "pytest")
