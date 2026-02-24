a1fa5004929992ff4ce503c71fb66a03a6482afd
Chris Richardson <chris@bpi.cam.ac.uk>
2026-02-24 10:47:50 +0000

    comma

--- spack_repo/fenics/packages/fenics_basix/package.py
+++ spack_repo/fenics/packages/fenics_basix/package.py
@@ -51,5 +51,5 @@ class FenicsBasix(CMakePackage):
     def cmake_args(self):
         return [
             "-DBLAS_LIBRARIES=" + self.spec["blas"].libs.joined(),
-            "-DLAPACK_LIBRARIES=" + self.spec["blas"].libs.joined()
+            "-DLAPACK_LIBRARIES=" + self.spec["blas"].libs.joined(),
         ]
